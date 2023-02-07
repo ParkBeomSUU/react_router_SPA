b16b8368702dd051e6c816a9d5f6fcc53a20c772
beomsu <pps9506@naver.com>
2023-02-07 16:46:39 +0900

    리액트 라우터로 SPA 개발하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes } from 'react-router-dom'
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Article from './pages/Article';
+import Articles from './pages/Articles';
+import Layout from './Layout';
+import MyPage from './pages/Mypages';
+import Login from './pages/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App =() => {
+  return(
+    <Routes>
+      <Route path="/" element={<Layout />} >
+      <Route index element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+        <Route path='/profiles/:username' element={<Profile/>} />
+        </Route>
+
+        <Route path='/articles' element={<Articles/>} /> 
+        <Route path='/:id' element={<Article/>} /> 
+        
+        <Route path='/login' element={<Login/>} /> 
+        <Route path='/mypage' element={<mypage/>} /> 
+
+
+    </Routes>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* 리액트 라우터를 적용할때는 index.js에서 react-router-dom에 내장되어있는
+  browserRouter,라는 컴포넌트를 사용하여 감싸면 된다.
+  이렇게 하면 새로 불러오지 않고도 주소를 변경하고 현재 주소의 경로에 관련된정보를 
+  리액트 펌토넌트에서 사용하게 해준다.
+
+*/
